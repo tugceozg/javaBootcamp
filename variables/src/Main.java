9dddbd56f9468e7a90e08c48fda84a47b39aad39
tugceozg <tugceozgenetu@gmail.com>
2022-10-23 13:51:11 +0300

    HW2

---
+++ variables/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ogrenciSayisi = 12;
+        String mesaj = "Ogrenci sayisi : ";
+        System.out.println(mesaj + ogrenciSayisi);
+
+    }
+}
