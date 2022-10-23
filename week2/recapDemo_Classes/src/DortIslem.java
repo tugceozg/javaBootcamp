8dc961d2598984d65bd3a821caaa3324366cfa84
tugceozg <tugceozgenetu@gmail.com>
2022-10-23 15:21:29 +0300

    Week2 Homework2

---
+++ week2/recapDemo_Classes/src/DortIslem.java
@@ -0,0 +1,14 @@
+public class DortIslem {
+    public int toplama(int sayi1, int sayi2){
+        return sayi1 + sayi2;
+    }
+    public int cikarma(int sayi1, int sayi2){
+        return sayi1-sayi2;
+    }
+    public int carpa(int sayi1, int sayi2){
+        return sayi1*sayi2;
+    }
+    public int bolme(int sayi1, int sayi2){
+        return sayi1/sayi2;
+    }
+}
